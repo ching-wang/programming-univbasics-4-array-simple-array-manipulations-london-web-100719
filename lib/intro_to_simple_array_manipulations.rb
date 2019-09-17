c10ae7111a0536fff7330d2e58b1b63dce4e36fe
Qing Wang <chingwang.qw@gmail.com>
2019-09-17 16:25:37 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,16 @@
+def using_push(array, atring)
+    array.push(string)
+end
+
+def using_unshift(array, string)
+   array.unshift(string)
+end 
+
+def using_pop(array)
+    array.pop()
+end 
+
+def pop_with_args(array)
+    newArray = array.pop()
+end 
+
